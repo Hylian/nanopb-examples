443f9bde5d5d8822e1f24e8560c6fbd296ef440b
Edward Shin <contact@edwardsh.in>
2020-05-05 18:40:03 -0400

    Initial commit with basic example

---
+++ 1_basic/decode.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "pb_common.h"
+#include "pb_decode.h"
+#include "basic.pb.h"
+
+int main(void) {
+  // Read the encoded output from the protoc generated output
+  FILE *test_file = fopen("build/basic_test.bin", "rb");
+  if (!test_file) {
+    printf("Unable to open test file!\n");
+    return 1;
+  }
+  char in_buf[200] = {0};
+  size_t in_len = 0;
+  while (fread(&in_buf[in_len], 1, 1, test_file) == 1) {
+    in_len++;
+  }
+  fclose(test_file);
+
+  // Create istream from the buffer
+  pb_istream_t istream = pb_istream_from_buffer(in_buf, in_len);
+
+  // Create camera struct to hold decoded values
+  hylian_camera_Camera out_camera = {0};
+
+  // Decode the test input into a struct
+  if (!pb_decode(&istream, hylian_camera_Camera_fields, &out_camera)) {
+    printf("pb_decode returned failure!\n");
+    return 1;
+  }
+
+  printf("Decode success!\n");
+
+  // Test the results to make sure they're what we expect
+  hylian_camera_Camera expected_camera = {
+    .name = "Leica M5",
+    .available = true,
+    .type = hylian_camera_Camera_CameraType_RANGEFINDER,
+  };
+
+  if (strcmp(expected_camera.name, out_camera.name) ||
+      expected_camera.available != out_camera.available ||
+      expected_camera.type != out_camera.type) {
+    printf("FAIL: Output does not match test file!\n");
+    return 1;
+  }
+
+  printf("PASS: Output matches test file!\n");
+
+  return 0;
+}
---
+++ 1_basic/encode.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "pb_common.h"
+#include "pb_encode.h"
+#include "basic.pb.h"
+
+int main(void) {
+  // Create the protobuf struct to encode
+  hylian_camera_Camera my_camera = {
+    .name = "Leica M5",
+    .available = true,
+    .type = hylian_camera_Camera_CameraType_RANGEFINDER,
+  };
+
+  char out_buf[200] = {0};
+
+  // Create an output stream from a buffer
+  pb_ostream_t ostream = pb_ostream_from_buffer(out_buf, sizeof(out_buf));
+
+  if (!pb_encode(&ostream, hylian_camera_Camera_fields, &my_camera)) {
+    printf("pb_encode returned failure!\n");
+    return 1;
+  }
+
+  printf("Encode success!\n");
+
+  // Test the encoded output against the protoc generated output
+  FILE *test_file = fopen("build/basic_test.bin", "rb");
+  if (!test_file) {
+    printf("Unable to open test file!\n");
+    return 1;
+  }
+
+  char test_buf[200] = {0};
+  fread(test_buf, ostream.bytes_written, 1, test_file);
+  fclose(test_file);
+  if (memcmp(out_buf, test_buf, ostream.bytes_written)) {
+    printf("FAIL: Output does not match test file!\n");
+    return 1;
+  }
+  printf("PASS: Output matches test file!\n");
+
+  return 0;
+}
